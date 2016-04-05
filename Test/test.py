a769efaf61f93139d0e7474d16694360720e9f8d
ycc1107 <cy699@nyu.edu>
2016-04-05 19:36:55 -0400

    test
    calculate std for aapl

--- Test/test.py
+++ Test/test.py
@@ -1,8 +1,23 @@
+import csv
+
+def readInData():
+    with open('aaplStockPrice.csv','rb') as f:
+        reader = csv.reader(f)
+        adjClosePrice = [ float( line[6] ) for i,line in enumerate(reader) if i != 0 ]
+    return adjClosePrice
+
+def std( adjClosePrice ):
+    n =  len(adjClosePrice) * 1.0
+    mean = sum(adjClosePrice) / n
+    std = ( sum( [ (price - mean)**2 for price in adjClosePrice ] ) / n ) **0.5
+    return std
+
 def main():
     '''
     testing function
     :return:
     '''
-    print 'Hello Project'
+    adjClosePrice = readInData()
+    print std(adjClosePrice)
 
 if __name__ == '__main__':main()
